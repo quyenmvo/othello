f2e1c205a709e2c32f53c5acb931c57f097b4fdb
quyenmvo <167739199+quyenmvo@users.noreply.github.com>
2025-05-10 16:38:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const lang_tweet_str_5_win = '';
 const lang_tweet_str_5_lose = '';
 const lang_tweet_str_5_draw = '';
 const lang_tweet_result = 'Tweet this Result!';
-const lang_ai_loading = "🤖 Higo is waking up...";
+const lang_ai_loading = "🤖 Higo is waking up... Please wait a moment!";
 const lang_ai_loaded = 'Higo is ready to play!';
 const lang_ai_load_failed = 'Oops! Higo didn’t wake up. Try reloading the page.';
 
